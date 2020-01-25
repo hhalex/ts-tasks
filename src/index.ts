6606d73a80c3b2c305ab03b484771f19588d5d97
Alexandre Careil <alex.careil.fr@gmail.com>
2020-01-25 16:47:56 +0100

    🎨 Create Task and TaskCombinator modules

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import { race, Task, createEventTaskCreator, createTimeoutTaskCreator } from "./lib";
+import { TaskCombinator, Task } from "./lib";
 
 const myAction = () => console.log("myAction was executed");
 
+const createWindowEventTask = Task.eventCreator<WindowEventMap, Window>(window);
+const createHtmlElementTask = Task.eventCreator<HTMLElementEventMap, HTMLElement>(document.getElementById("test"));
 
-const createWindowEventTask = createEventTaskCreator<WindowEventMap, Window>(window);
-const createHtmlElementTask = createEventTaskCreator<HTMLElementEventMap, HTMLElement>(document.getElementById("test"));
+const createTimeoutTask = Task.timeoutCreator(window);
 
-const createTimeoutTask = createTimeoutTaskCreator(window);
-
-const onBeforeUnloadOrTimeout = (thresholdMs: number, action: () => void) => race(
+const onBeforeUnloadOrTimeout = (thresholdMs: number, action: () => void) => TaskCombinator.race(
     createTimeoutTask(action, thresholdMs),
     createWindowEventTask("beforeunload", action)
 );
--- src/lib.ts
+++ src/lib.ts
@@ -5,9 +5,11 @@ export type Task<T> = {
     flatMap:  <U>(f: ((t: T) => Task<U>)) => Task<U>
     run: <U>(then: (v: T) => U) => ScheduledTask
 }
-const noop = <_T, _U>() => () => {
+
+const doNothing = <_T, _U>() => () => {
     return undefined as unknown as _U;
 };
+
 const flatMapTask = <T, U>(t: Task<T>, f: ((t: T) => Task<U>)): Task<U> => {
     const taskU: Task<U> = {
         run: <V>(then: (v: U) => V = noop<U, V>()): ScheduledTask => {
@@ -26,29 +28,15 @@ const flatMapTask = <T, U>(t: Task<T>, f: ((t: T) => Task<U>)): Task<U> => {
     };
     return taskU;
 };
-export const createTask = <T>(action: () => T): Task<T> => {
-    const taskT = {
-        run: <U>(then: ((v: T) => U) = noop<T, U>()) => {
-            then(action());
-            return {
-                cancel: () => false
-            };
-        },
-        flatMap: <U>(f: (t: T) => Task<U>): Task<U> => flatMapTask<T, U>(taskT, f)
-    };
-    return taskT;
-};
-export const createTimeoutTaskCreator = (w: Window) =>
-    <T>(action: () => T, thresholdMs: number): Task<T> => {
+
+export module Task {
+
+    export const lambda = <T>(action: () => T): Task<T> => {
         const taskT = {
-            run: <U>(then: ((v: T) => U) = noop<T, U>()) => {
-                let executed = false;
-                const timeoutId = w.setTimeout(() => { const value = action(); then(value); executed = true; }, thresholdMs);
+            run: <U>(then: ((v: T) => U) = doNothing<T, U>()) => {
+                then(action());
                 return {
-                    cancel: () => {
-                        w.clearTimeout(timeoutId);
-                        return executed;
-                    }
+                    cancel: () => false
                 };
             },
             flatMap: <U>(f: (t: T) => Task<U>): Task<U> => flatMapTask<T, U>(taskT, f)
@@ -56,23 +44,63 @@ export const createTimeoutTaskCreator = (w: Window) =>
         return taskT;
     };
 
-type EventListenable<EventMap extends {[key in keyof EventMap]: Event}> = {
-    addEventListener: <K extends keyof EventMap>(eventName: K, action: (e: EventMap[K]) => void) => void,
-    removeEventListener: <K extends keyof EventMap>(eventName: K, action: (e: EventMap[K]) => void) => void,
-};
+    export const noop = lambda(doNothing<void, void>());
 
-export const createEventTaskCreator = <EM extends {[key in keyof EM]: Event}, EL extends EventListenable<EM>>(el: EL) =>
-    <K extends keyof EM, T>(eventName: K, action: (e: EM[K]) => T): Task<T> => {
+    export const timeoutCreator = (w: Window) =>
+        <T>(action: () => T, thresholdMs: number): Task<T> => {
+            const taskT = {
+                run: <U>(then: ((v: T) => U) = doNothing<T, U>()) => {
+                    let executed = false;
+                    const timeoutId = w.setTimeout(() => { const value = action(); then(value); executed = true; }, thresholdMs);
+                    return {
+                        cancel: () => {
+                            w.clearTimeout(timeoutId);
+                            return executed;
+                        }
+                    };
+                },
+                flatMap: <U>(f: (t: T) => Task<U>): Task<U> => flatMapTask<T, U>(taskT, f)
+            };
+            return taskT;
+        };
+
+    type EventListenable<EventMap extends {[key in keyof EventMap]: Event}> = {
+        addEventListener: <K extends keyof EventMap>(eventName: K, action: (e: EventMap[K]) => void) => void,
+        removeEventListener: <K extends keyof EventMap>(eventName: K, action: (e: EventMap[K]) => void) => void,
+    };
+
+    export const eventCreator = <EM extends {[key in keyof EM]: Event}, EL extends EventListenable<EM>>(el: EL) =>
+        <K extends keyof EM, T>(eventName: K, action: (e: EM[K]) => T): Task<T> => {
+            const taskT = {
+                run: <U>(then: ((v: T) => U) = doNothing<T, U>()) => {
+                    let executed = false;
+                    const doit = (e: EM[K]) => { const value = action(e); then(value); executed = true; };
+                    el.addEventListener(eventName, doit);
+                    return {
+                        cancel: () => {
+                            el.removeEventListener(eventName, doit);
+                            return executed;
+                        }
+                    };
+                },
+                flatMap: <U>(f: (t: T) => Task<U>): Task<U> => flatMapTask<T, U>(taskT, f)
+            };
+            return taskT;
+        };
+}
+export module TaskCombinator {
+    export const repeat = <T>(task: Task<T>, n: number): Task<T> =>
+        n === 1
+            ? task
+            : task.flatMap(_ => repeat(task, n - 1)) as Task<T>;
+
+    export const race = <T>(...tasks: Task<T>[]): Task<T> => {
         const taskT = {
-            run: <U>(then: ((v: T) => U) = noop<T, U>()) => {
-                let executed = false;
-                const doit = (e: EM[K]) => { const value = action(e); then(value); executed = true; };
-                el.addEventListener(eventName, doit);
+            run: <U>(then: ((v: T) => U) = doNothing<T, U>()) => {
+                const scheduledTasks = tasks.map(t => t.run(v => { const res = then(v); cancelAll(); return res; }));
+                const cancelAll = () => scheduledTasks.map(t => t.cancel()).reduce((acc, current) => acc || current, false);
                 return {
-                    cancel: () => {
-                        el.removeEventListener(eventName, doit);
-                        return executed;
-                    }
+                    cancel: cancelAll
                 };
             },
             flatMap: <U>(f: (t: T) => Task<U>): Task<U> => flatMapTask<T, U>(taskT, f)
@@ -80,41 +108,23 @@ export const createEventTaskCreator = <EM extends {[key in keyof EM]: Event}, EL
         return taskT;
     };
 
-export const repeat = <T>(task: Task<T>, n: number): Task<T> =>
-    n === 1
-        ? task
-        : task.flatMap(_ => repeat(task, n - 1)) as Task<T>;
-
-export const race = <T>(...tasks: Task<T>[]): Task<T> => {
-    const taskT = {
-        run: <U>(then: ((v: T) => U) = noop<T, U>()) => {
-            const scheduledTasks = tasks.map(t => t.run(v => { const res = then(v); cancelAll(); return res; }));
-            const cancelAll = () => scheduledTasks.map(t => t.cancel()).reduce((acc, current) => acc || current, false);
-            return {
-                cancel: cancelAll
-            };
-        },
-        flatMap: <U>(f: (t: T) => Task<U>): Task<U> => flatMapTask<T, U>(taskT, f)
+    export const all = <T>(...tasks: Task<T>[]): Task<T> => {
+        const taskT = {
+            run: <U>(then: ((v: T) => U) = doNothing<T, U>()) => {
+                let remainingTasks = tasks.length;
+                const scheduledTasks = tasks.map(t =>
+                    t.run(v => {
+                        if (remainingTasks-- == 0) {
+                            then(v);
+                        }
+                    })
+                );
+                return {
+                    cancel: () => scheduledTasks.map(t => t.cancel()).reduce((acc, current) => acc || current, false)
+                };
+            },
+            flatMap: <U>(f: (t: T) => Task<U>): Task<U> => flatMapTask<T, U>(taskT, f)
+        };
+        return taskT;
     };
-    return taskT;
-};
-
-export const all = <T>(...tasks: Task<T>[]): Task<T> => {
-    const taskT = {
-        run: <U>(then: ((v: T) => U) = noop<T, U>()) => {
-            let remainingTasks = tasks.length;
-            const scheduledTasks = tasks.map(t =>
-                t.run(v => {
-                    if (remainingTasks-- == 0) {
-                        then(v);
-                    }
-                })
-            );
-            return {
-                cancel: () => scheduledTasks.map(t => t.cancel()).reduce((acc, current) => acc || current, false)
-            };
-        },
-        flatMap: <U>(f: (t: T) => Task<U>): Task<U> => flatMapTask<T, U>(taskT, f)
-    };
-    return taskT;
-};
+}
