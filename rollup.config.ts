d20d8ca01d77718cbf1bf20e753cfa5367c86809
Alexandre Careil <alex.careil.fr@gmail.com>
2020-03-23 19:20:52 +0100

    🔨 Change module type to "es"

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,7 +3,7 @@ import typescript from 'rollup-plugin-typescript2';
 export default {
     input: `src/index.ts`,
     output: [
-      { file: "lib/index.js", name: "pledge-ts", format: 'umd', sourcemap: true }
+      { file: "lib/index.js", name: "pledge-ts", format: 'es', sourcemap: true }
     ],
     // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
     external: [],
